fae90a38ee3721206a395b03b587e6bc444adbe9
yuri <yuri@example.com>
2023-03-01 08:53:52 +0900

    Install dotenv and connect a DB ➕

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 3000;
+require("dotenv").config();
 
-app.get("/", (req, res) => {
-  res.send("Hello Express");
-});
+//DB接続・DB connection
+try {
+  //expected connection string to start with mongodb+srv:// using process.env
+  //https://www.mongodb.com/community/forums/t/unable-to-connect-db-because-of-throw-new-mongoparseerror-invalid-scheme-expected-connection-string-to-start-with-mongodb-or-mongodb-srv/176629/5
+  mongoose.connect(process.env.MONGODB_URL);
+  console.log("DBと接続中・Connecting to DB 🍏");
+} catch (error) {
+  console.log(error);
+}
 
 app.listen(PORT, () => {
-  console.log("ローカルサーバー起動中🚀");
+  console.log("ローカルサーバー起動中・Local server running 🚀");
 });
