0394f11cdc346cb923162d555312a29014fa0955
yuri <yuri@example.com>
2023-03-18 19:37:47 +0900

    ✅ 49. Get params from form

---
+++ client/src/api/axiosClient.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:3000/api/v1";
+const getToken = () => {
+  localStorage.getItem("token");
+};
+
+const axiosClient = axios.create({
+  baseURL: BASE_URL,
+});
+
+//APIを叩く前に前処理を行う
+axiosClient.interceptors.request.use(async (config) => {
+  return {
+    config,
+    header: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${getToken()}`, //リクエストヘッダにJWTをつけてサーバーに渡す
+    },
+  };
+});
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -4,9 +4,21 @@ import { LoadingButton } from "@mui/lab";
 import { Link } from "react-router-dom";
 
 const Register = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    //入力欄の文字列を取得
+    const data = new FormData(e.target);
+    const username = data.get("username").trim();
+    const password = data.get("password").trim();
+    const confirmPassword = data.get("confirmPassword").trim();
+    console.log(username);
+    console.log(password);
+    console.log(confirmPassword);
+  };
+
   return (
     <>
-      <Box component="form">
+      <Box component="form" onSubmit={handleSubmit}>
         <TextField
           fullWidth
           id="username"
