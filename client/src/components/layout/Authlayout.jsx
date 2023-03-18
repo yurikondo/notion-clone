48f431392dee9a97c4b3fa93fdfc148ca82fe593
yuri <yuri@example.com>
2023-03-18 17:54:31 +0900

    Add container and image 💄

--- client/src/components/layout/Authlayout.jsx
+++ client/src/components/layout/Authlayout.jsx
@@ -1,10 +1,27 @@
+import { Container, Box } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
+import notionLogo from "../../assets/images/notion-logo.png";
 
 const Authlayout = () => {
   return (
     <div>
-      Authlayout
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <img
+            src={notionLogo}
+            style={{ width: 100, height: 100, marginBottom: 3 }}
+          />
+          Notionクローン
+        </Box>
+      </Container>
       <Outlet />
     </div>
   );
