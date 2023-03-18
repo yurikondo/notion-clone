71b167ba435b7bbab7ff32036d5e58210c9307db
yuri <yuri@example.com>
2023-03-18 16:18:26 +0900

    Create components files ✅

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,8 @@
-import './App.css';
+import "./App.css";
+import Login from "./pages/Login";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
---
+++ client/src/components/layout/Authlayout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const authlayout = () => {
+  return (
+    <div>authlayout</div>
+  )
+}
+
+export default authlayout
