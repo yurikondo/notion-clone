c6781820beb7add4ab5a2a4177b3f211db15b367
yuri <yuri@example.com>
2023-03-19 15:47:23 +0900

    ✨ 59. Create Login Page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,110 @@
-import React from "react";
+import { Box, Button, TextField } from "@mui/material";
+import React, { useState } from "react";
+import { LoadingButton } from "@mui/lab";
+import { Link, useNavigate } from "react-router-dom";
+import authApi from "../api/authApi";
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const [usernameErrText, setUsernameErrText] = useState("");
+  const [passwordErrText, setPasswordErrText] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setUsernameErrText("");
+    setPasswordErrText("");
+
+    //入力欄の文字列を取得
+    const data = new FormData(e.target);
+    const username = data.get("username").trim();
+    const password = data.get("password").trim();
+    console.log(username);
+    console.log(password);
+
+    let error = false;
+
+    if (username === "") {
+      error = true;
+      setUsernameErrText("名前を入力してください");
+    }
+    if (password === "") {
+      error = true;
+      setPasswordErrText("パスワードを入力してください");
+    }
+    if (error) return;
+
+    setLoading(true);
+
+    //新規登録API
+    try {
+      const res = await authApi.register({
+        username,
+        password,
+      });
+      setLoading(false);
+      localStorage.setItem("token", res.token);
+      console.log("新規登録に成功しました🎉");
+      navigate("/");
+    } catch (err) {
+      console.log(`Register.jsxのエラー：${err}`);
+      const errors = err.data.errors;
+      console.log(errors);
+      errors.forEach((err) => {
+        if (err.param === "username") {
+          setUsernameErrText(err.msg);
+        }
+        if (err.param === "password") {
+          setPasswordErrText(err.msg);
+        }
+      });
+      setLoading(false);
+    }
+  };
+
   return (
-    <div>
-      Login
-    </div>
+    <>
+      <Box component="form" onSubmit={handleSubmit} noValidate>
+        <TextField
+          fullWidth
+          id="username"
+          label="お名前"
+          margin="normal"
+          name="username"
+          required
+          helperText={usernameErrText}
+          error={usernameErrText !== ""}
+          disabled={loading}
+        />
+        <TextField
+          fullWidth
+          id="password"
+          label="パスワード"
+          margin="normal"
+          name="password"
+          type="password"
+          required
+          helperText={passwordErrText}
+          error={passwordErrText !== ""}
+          disabled={loading}
+        />
+        <LoadingButton
+          sx={{ mt: 3, mb: 2 }}
+          fullWidth
+          type="submit"
+          color="primary"
+          variant="outlined"
+          loading={loading}
+        >
+          ログイン
+        </LoadingButton>
+      </Box>
+      <Button component={Link} to="/register">
+        アカウントを持っていませんか？新規登録
+      </Button>
+    </>
   );
 };
 
