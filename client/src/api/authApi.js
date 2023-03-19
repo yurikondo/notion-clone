abb6baaf1c4d5dd885bc87fc7d6ac0a2a77e5e88
yuri <yuri@example.com>
2023-03-19 16:26:35 +0900

    ✨ 63. Verify token

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -3,6 +3,7 @@ import axiosClient from "./axiosClient";
 const authApi = {
   register: (params) => axiosClient.post("auth/register", params),
   login: (params) => axiosClient.post("auth/login", params),
+  verifyToken: () => axiosClient.post("auth/varify-token"),
 };
 
 export default authApi;
--- client/src/components/layout/Authlayout.jsx
+++ client/src/components/layout/Authlayout.jsx
@@ -1,9 +1,24 @@
 import { Container, Box } from "@mui/material";
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import notionLogo from "../../assets/images/notion-logo.png";
+import authUtils from "../../utils/authUtils";
 
 const Authlayout = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    //JWTを持っているのか確認する
+    const checkAuth = async () => {
+      //認証チェック
+      const isAuth = await authUtils.isAuthenticated();
+      if (isAuth) {
+        navigate("/");
+      }
+    };
+    checkAuth();
+  }, [navigate]);
+
   return (
     <div>
       <Container component="main" maxWidth="xs">
