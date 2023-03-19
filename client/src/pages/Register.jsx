04fdb34f9f574a317418646f1b5c3a132baa58d1
yuri <yuri@example.com>
2023-03-19 15:33:04 +0900

    ✨ 57. Redirect to Home page from Register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,10 +1,12 @@
 import { Box, Button, TextField } from "@mui/material";
 import React, { useState } from "react";
 import { LoadingButton } from "@mui/lab";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import authApi from "../api/authApi";
 
 const Register = () => {
+  const navigate = useNavigate();
+
   const [usernameErrText, setUsernameErrText] = useState("");
   const [passwordErrText, setPasswordErrText] = useState("");
   const [confirmPasswordErrText, setConfirmPasswordErrText] = useState("");
@@ -58,6 +60,7 @@ const Register = () => {
       setLoading(false);
       localStorage.setItem("token", res.token);
       console.log("新規登録に成功しました🎉");
+      navigate("/");
     } catch (err) {
       console.log(`Register.jsxのエラー：${err}`);
       const errors = err.data.errors;
