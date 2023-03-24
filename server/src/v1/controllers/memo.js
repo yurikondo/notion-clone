a0ac7fc63210cca027e5c5bad332fdfca79ae61e
yuri <yuri@example.com>
2023-03-25 06:25:15 +0900

    ✨ 77.Create memo API

---
+++ server/src/v1/controllers/memo.js
@@ -0,0 +1,15 @@
+const Memo = require("../models/memo");
+
+exports.create = async (req, res) => {
+  try {
+    const memoCount = await Memo.find().count();
+    //メモ新規作成
+    const memo = await Memo.create({
+      user: req.user._id,
+      position: memoCount > 0 ? memoCount : 0,
+    });
+    res.status(201).json(memo);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
