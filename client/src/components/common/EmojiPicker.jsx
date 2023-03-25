2e261d79ea62ef28eec656102d76e33189b48103
yuri <yuri@example.com>
2023-03-25 16:45:08 +0900

    💄 99.Add Emoji Icon

--- client/src/components/common/EmojiPicker.jsx
+++ client/src/components/common/EmojiPicker.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React, { useEffect, useState } from "react";
 
 const EmojiPicker = (props) => {
-  return <div>{props.icon}</div>;
+  const [selectedEmoji, setSelectedEmoji] = useState();
+  useEffect(() => {
+    setSelectedEmoji(props.icon);
+  }, [props.icon]);
+
+  return (
+    <Box>
+      <Typography variant="h3" fontWeight="700" sx={{ cursor: "pointer" }}>
+        {selectedEmoji}
+      </Typography>
+    </Box>
+  );
 };
 
 export default EmojiPicker;
