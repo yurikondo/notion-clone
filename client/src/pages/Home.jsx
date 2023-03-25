bec6223ceebc5a1d2b740192372cca9de55faac7
yuri <yuri@example.com>
2023-03-25 09:18:59 +0900

    ✨ 81.Create memo in mongoDB

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,23 @@
 import React, { useState } from "react";
 import { LoadingButton } from "@mui/lab";
 import { Box } from "@mui/material";
+import memoApi from "../api/memoApi";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
-  const createMemo = () => {};
+  const createMemo = async () => {
+    try {
+      setLoading(true);
+      const res = await memoApi.create();
+      navigate(`/memo/${res._id}`);
+    } catch (error) {
+      alert(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <Box
@@ -18,7 +31,7 @@ function Home() {
       <LoadingButton
         variant="outlined"
         color="success"
-        onClick={() => createMemo}
+        onClick={() => createMemo()}
         loading={loading}
       >
         最初のメモを作成
