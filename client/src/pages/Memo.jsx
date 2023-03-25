b424faf683be3b0fec742d315ab21cb25945a95d
yuri <yuri@example.com>
2023-03-25 13:03:45 +0900

    💄 89.Modify input

--- client/src/pages/Memo.jsx
+++ client/src/pages/Memo.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import {
   TextField,
-  List,
-  ListItemButton,
   Box,
-  Typography,
   IconButton,
 } from "@mui/material";
 import StarBoderOutlined from "@mui/icons-material/StarBorderOutlined";
@@ -17,7 +14,6 @@ function Memo() {
         sx={{
           display: "flex",
           alignItems: "center",
-          justifyContent: "space-between",
           width: "100%",
         }}
       >
@@ -27,11 +23,31 @@ function Memo() {
         <IconButton variant="outlined" color="error">
           <DeleteOutlined />
         </IconButton>
-        Memo
       </Box>
       <Box sx={{ padding: "10px 50px" }}>
-        <TextField placeholder="無題" variant="outlined" fullWidth />
-        <TextField placeholder="追加" variant="outlined" fullWidth />
+        <TextField
+          placeholder="無題"
+          variant="outlined"
+          fullWidth
+          sx={{
+            ".MuiOutlinedInput-input": { padding: 0 },
+            ".MuiOutlinedInput-notchedOutline": { border: "none" },
+            ".MuiOutlinedInput-root": { fontSize: "2rem", fontWeight: "700" },
+          }}
+        />
+        <TextField
+          placeholder="追加"
+          variant="outlined"
+          fullWidth
+          placeholder="無題"
+          variant="outlined"
+          fullWidth
+          sx={{
+            ".MuiOutlinedInput-input": { padding: 0 },
+            ".MuiOutlinedInput-notchedOutline": { border: "none" },
+            ".MuiOutlinedInput-root": { fontSize: "1rem" },
+          }}
+        />
       </Box>
     </>
   );
