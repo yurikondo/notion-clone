a407417a7cd59a2dec9e3f8aad523cb8a40cc16c
yuri <yuri@example.com>
2023-03-25 12:47:14 +0900

    💄 88.Add icons and inputs

--- client/src/pages/Memo.jsx
+++ client/src/pages/Memo.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import {
+  TextField,
+  List,
+  ListItemButton,
+  Box,
+  Typography,
+  IconButton,
+} from "@mui/material";
+import StarBoderOutlined from "@mui/icons-material/StarBorderOutlined";
+import DeleteOutlined from "@mui/icons-material/DeleteOutlined";
 
 function Memo() {
   return (
-    <div>Memo</div>
-  )
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          width: "100%",
+        }}
+      >
+        <IconButton>
+          <StarBoderOutlined />
+        </IconButton>
+        <IconButton variant="outlined" color="error">
+          <DeleteOutlined />
+        </IconButton>
+        Memo
+      </Box>
+      <Box sx={{ padding: "10px 50px" }}>
+        <TextField placeholder="無題" variant="outlined" fullWidth />
+        <TextField placeholder="追加" variant="outlined" fullWidth />
+      </Box>
+    </>
+  );
 }
 
-export default Memo
+export default Memo;
