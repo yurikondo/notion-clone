bbed1753f2419428bae66a42c117483d871bdf68
yuri <yuri@example.com>
2023-03-25 17:17:33 +0900

    💄 101.Control picker

--- client/src/components/common/EmojiPicker.jsx
+++ client/src/components/common/EmojiPicker.jsx
@@ -5,16 +5,33 @@ import Picker from "@emoji-mart/react";
 
 const EmojiPicker = (props) => {
   const [selectedEmoji, setSelectedEmoji] = useState();
+  const [isShowPicker, setIsShowPicker] = useState(false);
+
   useEffect(() => {
     setSelectedEmoji(props.icon);
   }, [props.icon]);
 
+  const showPicker = () => {
+    setIsShowPicker(!isShowPicker);
+  };
+
   return (
     <Box>
-      <Typography variant="h3" fontWeight="700" sx={{ cursor: "pointer" }}>
+      <Typography
+        variant="h3"
+        fontWeight="700"
+        sx={{ cursor: "pointer" }}
+        onClick={showPicker}
+      >
         {selectedEmoji}
       </Typography>
-      <Box sx={{ display: "none" }}>
+      <Box
+        sx={{
+          display: isShowPicker ? "block" : "none",
+          position: "absolute",
+          zIndex: "100",
+        }}
+      >
         <Picker />
       </Box>
     </Box>
